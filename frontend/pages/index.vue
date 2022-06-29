60697f064d6e9ded6aed486f147a71935817a4e8
BTX_ON_YT <jappleid441@gmail.com>
2022-06-29 17:25:22 +1200

    idk

---
+++ frontend/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="App">
+      <header class="App-header">
+        <span class="App-head-link"><a class="App-link2" href="vps">Vps</a></span>
+        
+      </header>
+
+      <h1 class="App-title">Vps's For The 21st Century</h1>
+    </div>
+  </div>
+</template>
+
+
+<style scope>
+@import "/App.css";
+</style>
---
+++ frontend/pages/vps.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="App">
+      <header class="App-header">
+        <span class="App-head-link"><NuxtLink class="App-link2" href="/vps">Vpsf</NuxtLink></span>
+        <span class="App-head-link"><NuxtLink class="App-link2" href="http://"> Test</NuxtLink></span>
+      </header>
+
+      
+      <center style="padding-top: 2em;">
+        <div class="list">
+          <h1 class="App-title">Vps Pricing</h1>
+          <ol class="list-2">
+            <li class="list-item"><strong>$4/mo</strong> | <strong>Ram:</strong>512mb | <strong>VCpu Cores:</strong>1 | <strong>Storage:</strong>10gb | <strong>Bandwidth:</strong>0.5tb</li>
+            <li class="list-item"><strong>$6/mo</strong> | <strong>Ram:</strong>1gb | <strong>VCpu Cores:</strong>1 | <strong>Storage:</strong>25gb | <strong>Bandwidth:</strong>2tb</li>
+            <li class="list-item"><strong>$12/mo</strong> | <strong>Ram:</strong>2gb | <strong>VCpu Cores:</strong>1 | <strong>Storage:</strong>50gb | <strong>Bandwidth:</strong>3tb</li>
+            <li class="list-item"><strong>$18/mo</strong> | <strong>Ram:</strong>2gb | <strong>VCpu Cores:</strong>2 | <strong>Storage:</strong>60gb | <strong>Bandwidth:</strong>4tb</li>
+            <li class="list-item"><strong>$24/mo</strong> | <strong>Ram:</strong>4gb | <strong>VCpu Cores:</strong>2 | <strong>Storage:</strong>100gb | <strong>Bandwidth:</strong>5tb</li>
+            <li class="list-item"><strong>$48/mo</strong> | <strong>Ram:</strong>8gb | <strong>VCpu Cores:</strong>2 | <strong>Storage:</strong>180gb | <strong>Bandwidth:</strong>6tb</li>
+          </ol>
+        </div>
+      </center>
+    </div>
+  </div>
+</template>
+
+
+<style scope>
+@import "/App.css";
+</style>
